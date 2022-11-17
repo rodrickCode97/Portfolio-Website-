7799fe6004778e79ff1a04a237cd08fb76cd70e5
rodrickCode97 <116767605+rodrickCode97@users.noreply.github.com>
2022-11-16 19:08:23 -0600

    Portfolio Website verison 1.0.4

--- main.js
+++ main.js
@@ -5,12 +5,33 @@ Granite Gray #666666
 Steel Blue #58819D
 Black Coral #4B5E6C
 */
+
+
+
+let headText = document.getElementsByClassName('display-4')[1];
+let headText1 = document.getElementsByClassName('display-4')[2];
+let headText2 = document.getElementsByClassName('display-4')[3];
+let headText3 = document.getElementsByClassName('display-4')[4];
+let jumbotron = document.getElementById('jumbotrons');
+let navLogo = document.getElementById('navBrand');
 let colors = ['#A9CEF4', '#6F919B', '#666666', '#58819D','#4B5E6C'];
 
-newColor = colors[Math.floor(Math.random() * 5)];
+jumbotron.addEventListener('mouseover', function() {
+    jumbotron.style.color = colors[Math.floor(Math.random()* 5)];
+});
+headText.addEventListener('mouseover', function(){
+    headText.style.color = colors[Math.floor(Math.random() * 5)];
+});
+headText1.addEventListener('mouseover', function(){
+    headText1.style.color = colors[Math.floor(Math.random() * 5)];
+});
+headText2.addEventListener('mouseover', function(){
+    headText2.style.color = colors[Math.floor(Math.random() * 5)];
+});
+headText3.addEventListener('mouseover', function(){
+    headText3.style.color = colors[Math.floor(Math.random() * 5)];
+});
 
-const div = document.getElementsByClassName('container')[1];
-
-div.addEventListener('mouseover', (event)=>{
-event.target.style.color = newColor;
+navLogo.addEventListener('mouseover', function (){
+    navLogo.style.color = colors[Math.floor(Math.random()* 5)];
 });
